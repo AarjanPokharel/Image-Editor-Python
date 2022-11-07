0dbdbbe30989c91615bd78817285df88c71dec9c
AarjanPokharel <104973899+AarjanPokharel@users.noreply.github.com>
2022-11-07 17:42:37 +0545

    first commit

---
+++ imgeditor.py
@@ -0,0 +1,24 @@
+from PIL import Image, ImageEnhance, ImageFilter
+import os
+
+path  = './imgs'
+pathOut = '/editedImgs'
+
+for filename in os.listdir(path):
+    img = Image.open(f"{path}/{filename}")
+
+    edit = img.filter(ImageFilter.SHARPEN).convert('L').rotate(-90)
+
+    factor = float(input("Enter the contrast factor:"))
+
+    enhancer = ImageEnhance.Contrast(edit)
+
+    edit = enhancer.enhance(factor)
+
+    clean_name = os.path.splitext(filename)[0]
+
+    edit.save(f'.{pathOut}/{clean_name}_edited.jpg')
+    
+
+
+
